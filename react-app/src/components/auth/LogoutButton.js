d13540590344371727a4760bf53079b7f70c9ccc
ethan <eman5797@gmail.com>
2023-03-08 02:52:07 -0500

    fixed stale values upon logout/login

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useDispatch } from 'react-redux';
+import { OptionContext } from '../../context/OptionContext';
 import { clearNotes } from '../../store/note';
 import { clearNotebooks } from '../../store/notebook';
 import { logout } from '../../store/session';
@@ -7,11 +8,14 @@ import { clearTags } from '../../store/tag';
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
+  const { setOption } = useContext(OptionContext);
+
   const onLogout = async (e) => {
     await dispatch(logout());
     await dispatch(clearNotebooks());
     await dispatch(clearNotes());
     await dispatch(clearTags());
+    setOption('home');
   };
 
   return <button className='edit-button' onClick={onLogout}><i class="fa-solid fa-right-from-bracket edit-pencil"></i></button>;
